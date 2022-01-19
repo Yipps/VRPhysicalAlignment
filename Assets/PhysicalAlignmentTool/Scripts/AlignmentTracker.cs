09dc0a933aad81061f36a793cb00dce0ca2678b5
Lawrence Yip <lawrenceyip1@gmail.com>
2022-01-19 14:17:13 -0500

    Revert "removed unused files"
    This reverts commit 36ee893979fc6215dcf8abdc67ad2a8126b04ffa.

--- Assets/PhysicalAlignmentTool/Scripts/AlignmentTracker.cs
+++ Assets/PhysicalAlignmentTool/Scripts/AlignmentTracker.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using Gizmos = Popcron.Gizmos;
 
 public class AlignmentTracker : MonoBehaviour
 {
